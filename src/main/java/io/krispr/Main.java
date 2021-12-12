3dd40e7797d56e1644cd88a616fff1558396529f
Tom Lynch <lync0143@gmail.com>
2021-12-12 07:31:37 -0600

    Add tests and ignore .idea directory.

--- src/main/java/io/krispr/Main.java
+++ src/main/java/io/krispr/Main.java
@@ -3,6 +3,10 @@ package io.krispr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Greet visitors using an environment variable.
+ */
+
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
